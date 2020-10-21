e5421a1a36ca11c4107b279277dcaf24cc657671
pavilion1900 <pavilion1900@gmail.com>
2020-10-21 13:02:35 +0300

    6. Date. Отображение даты. [#373887]

---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,12 @@
+package ru.job4j.tracker;
+
+import java.time.format.DateTimeFormatter;
+
+public class StartUI {
+    public static void main(String[] args) {
+        Item first = new Item();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
+        String currentDateTimeFormat = first.getCreated().format(formatter);
+        System.out.println("Текущие дата и время: " + currentDateTimeFormat);
+    }
+}
