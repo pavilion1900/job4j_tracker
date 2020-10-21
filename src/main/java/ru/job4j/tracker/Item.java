35147b3bc0adaa01a416e1dc316e303c9e2e69f8
pavilion1900 <pavilion1900@gmail.com>
2020-10-21 19:44:59 +0300

    2. Перегрузка конструктора [#373890]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -5,13 +5,23 @@ import java.time.LocalDateTime;
 public class Item {
     private int id;
     private String name;
-
     private LocalDateTime created = LocalDateTime.now();
+
+    public Item() {
+
+    }
+
+    public Item(int id) {
+
+    }
+
+    public Item(int id, String name) {
+
+    }
 
     public int getId() {
         return id;
     }
-
     public void setId(int id) {
         this.id = id;
     }
@@ -19,7 +29,6 @@ public class Item {
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
