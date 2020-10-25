7aae8f4c7cf494921ba6b0c4159e11656e935e4f
pavilion1900 <pavilion1900@gmail.com>
2020-10-25 08:38:01 +0300

    6. Метод замены заявки. Tracker.replace [#373902]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -49,6 +49,7 @@ public class Tracker {
     public boolean replace(int id, Item item) {
         int index = indexOf(id);
         if (index >= 0) {
+            items[index].setId(items[index].getId());
             items[index].setName(item.getName());
         }
         return index >= 0;
