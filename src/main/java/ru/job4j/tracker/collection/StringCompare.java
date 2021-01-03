661fe0cd33f1cf28a55ecfa04d0533ba7dd8b548
pavilion1900 <pavilion1900@gmail.com>
2021-01-03 15:10:33 +0300

    3. Компаратор для строк [#373799]

---
+++ src/main/java/ru/job4j/tracker/collection/StringCompare.java
@@ -0,0 +1,20 @@
+package ru.job4j.tracker.collection;
+
+import java.util.Comparator;
+
+public class StringCompare implements Comparator<String> {
+    @Override
+    public int compare(String first, String second) {
+        int rsl = 0;
+        for (int i = 0; i < first.length() && i < second.length(); i++) {
+            if (first.charAt(i) != second.charAt(i)) {
+                rsl += first.charAt(i) - second.charAt(i);
+                break;
+            }
+        }
+        if (rsl == 0) {
+            rsl = first.length() - second.length();
+        }
+        return rsl;
+    }
+}
