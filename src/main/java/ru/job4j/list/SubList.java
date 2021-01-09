809a49abc3d8f1f65fa4530ff31a27ecad1cc1b2
pavilion1900 <pavilion1900@gmail.com>
2021-01-09 22:36:50 +0300

    9. Получить часть списка с определенными индексами

---
+++ src/main/java/ru/job4j/list/SubList.java
@@ -0,0 +1,14 @@
+package ru.job4j.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SubList {
+    public static List<String> getElementsBetweenIndexes(List<String> list, String el) {
+        if (!list.contains(el) || (list.indexOf(el) == list.lastIndexOf(el))) {
+            return new ArrayList<>();
+        } else {
+            return list.subList(list.indexOf(el), list.lastIndexOf(el));
+        }
+    }
+}
