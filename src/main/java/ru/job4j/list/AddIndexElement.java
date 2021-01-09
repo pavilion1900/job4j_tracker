fd8e0d4b8867a62eff405104a37237ed7bcbf81c
pavilion1900 <pavilion1900@gmail.com>
2021-01-09 20:01:40 +0300

    3. Добавление элемента в список по индексу

---
+++ src/main/java/ru/job4j/list/AddIndexElement.java
@@ -0,0 +1,14 @@
+package ru.job4j.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddIndexElement {
+    public static boolean addNewElement(List<String> list, int index, String str) {
+        List<String> check = new ArrayList<>(list);
+        if (!check.contains(str)) {
+            check.add(index, str);
+        }
+        return check.size() != list.size();
+    }
+}
