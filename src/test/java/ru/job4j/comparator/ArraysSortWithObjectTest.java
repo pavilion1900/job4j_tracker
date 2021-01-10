423ad66ff9bbe2239faf39e5b84ae1434d95e912
pavilion1900 <pavilion1900@gmail.com>
2021-01-10 23:21:24 +0300

    2.2. Способы сортировки. Сортировка массива объектов

---
+++ src/test/java/ru/job4j/comparator/ArraysSortWithObjectTest.java
@@ -0,0 +1,17 @@
+package ru.job4j.comparator;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ArraysSortWithObjectTest {
+    @Test
+    public void test() {
+        String[] input = {"a", "b", "c"};
+        String[] result = ArraysSortWithObject.sort(input);
+        String[] expect = {"c", "b", "a"};
+        Assert.assertArrayEquals(expect, result);
+    }
+
+}
