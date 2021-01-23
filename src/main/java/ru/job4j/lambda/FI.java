8a2fa2e5f846916a17e774f07268a7214d5a666c
pavilion1900 <pavilion1900@gmail.com>
2021-01-23 22:06:04 +0300

    2.2. Лямбда блок [#373631]

--- src/main/java/ru/job4j/lambda/FI.java
+++ src/main/java/ru/job4j/lambda/FI.java
@@ -1,10 +1,7 @@
 package ru.job4j.lambda;
 
-import java.io.InputStream;
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.List;
-import java.util.function.Function;
 
 public class FI {
     public static void main(String[] args) {
@@ -13,26 +10,22 @@ public class FI {
                 new Attachment("image 3", 120),
                 new Attachment("image 2", 23)
         };
-        Comparator<Attachment> comparator = new Comparator<>() {
-            @Override
-            public int compare(Attachment o1, Attachment o2) {
-                return o1.getName().compareTo(o2.getName());
-            }
+        Comparator<Attachment> cmpDescName = (left, right) -> {
+            System.out.println("compare - " + right.getName() + " : " + left.getName());
+            return right.getName().compareTo(left.getName());
         };
-        Arrays.sort(atts, comparator);
+
+        Comparator<Attachment> cmpDescSize = (left, right) -> {
+            System.out.println("compare - " + right.getSize() + " : " + left.getSize());
+            return Integer.compare(right.getSize(), left.getSize());
+        };
+        Arrays.sort(atts, cmpDescName);
         for (Attachment attachment : atts) {
             System.out.println(attachment);
         }
-        List<Attachment> list = Arrays.asList(
-                new Attachment("image 1", 100),
-                new Attachment("image 2", 34),
-                new Attachment("image 3", 13)
-        );
-    }
-
-    public static void raw(List<Attachment> list, Function<Attachment, InputStream> func) {
-        for (Attachment att : list) {
-            func.apply(att);
+        Arrays.sort(atts, cmpDescSize);
+        for (Attachment attachment : atts) {
+            System.out.println(attachment);
         }
     }
 }
